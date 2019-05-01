5732ff687449e7e5ce3f7f79dd4a460b66afc4f1
Mikkel Rath Pedersen <mrp@teknologisk.dk>
2019-05-01 12:18:24 +0200

    changed element_count to property

--- src/pattern_manager/collection/manager.py
+++ src/pattern_manager/collection/manager.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+
+# Copyright 2019 Danish Technological Institute (DTI)
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Mads Vainoe Baatrup
+
 from bidict import bidict
 
 
@@ -44,14 +62,14 @@ class Manager(object):
 
     def get_next_element(self):
         next_i = self.iterator + 1
-        if next_i < self.element_count():
+        if next_i < self.element_count:
             return next_i
         else:
             return False
 
     def increase_iterator(self):
         next_i = self.iterator + 1
-        if next_i < self.element_count():
+        if next_i < self.element_count:
             self.iterator += 1
         else:
             self.finished = True
@@ -59,6 +77,7 @@ class Manager(object):
         
         return next_i
 
+    @property
     def element_count(self):
         return len(self.elements)
 
