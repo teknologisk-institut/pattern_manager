64a29c486c69e701d9de16237a6326181a956d0d
Nicola Conti <nicola@sysdesign.it>
2021-11-22 08:45:26 +0100

    Add Honeycomb pattern plugin

---
+++ src/pattern_manager/plugins/pattern_honeycomb.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+# Copyright 2019 Danish Technological Institute (DTI)
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Nicola Conti
+
+import rospy
+import numpy as np
+
+from pattern_manager.plugin import Plugin
+from pattern_manager.util import handle_input_1d
+from pattern_manager.xform import XForm
+
+
+class HoneycombPattern(Plugin):
+    """
+    This plugin class specifies the attributes of, and is responsible for the generation of, an honeycomb XForm pattern
+
+    :param parent: An XForm parent object under which to create the XForm pattern
+    :type parent: XForm
+    :param num_points: The number of points which make up the pattern
+    :type num_points: list, optional
+    :param step_sizes: The distance between each XForm's position in the pattern
+    :type step_sizes: list, optional
+    :param line_lens: The total length of the pattern
+    :type line_lens: list, optional
+    """
+
+    def __init__(self, parent, num_points=(0, 0), step_sizes=(0, 0), line_lens=(0.0, 0.0)):
+        super(HoneycombPattern, self).__init__()
+
+        self.parent = parent
+        self.num_points = num_points
+        self.step_sizes = step_sizes
+        self.line_lens = line_lens
+
+    def process(self):
+        """
+        This function generates the XForm pattern from the instance attributes
+        """
+
+        try:
+            (po_x, st_x, le_x) = handle_input_1d(self.num_points[0], self.step_sizes[0], self.line_lens[0])
+            (po_y, st_y, le_y) = handle_input_1d(self.num_points[1], self.step_sizes[1], self.line_lens[1])
+        except TypeError, e:
+            rospy.logerr(e)
+
+            return None
+
+        xy_set = set()
+
+        # for i in range(po_x):
+        #     for j in range(po_y):
+        #         xy_set.add((i * st_x, j * st_y))
+        #
+        # for i in range(po_x - 1):
+        #     for j in range(po_y - 1):
+        #         xy_set.add((i * st_x + st_x/2, j * st_y + st_y/2))
+
+        for of_x in np.arange(0., le_x, st_x):
+            for of_y in np.arange(0., le_y, st_y):
+                xy_set.add((of_x, of_y))
+
+        for of_x in np.arange(st_x/2., le_x, st_x):
+            for of_y in np.arange(st_y/2., le_y, st_y):
+                xy_set.add((of_x, of_y))
+
+        tfs = []
+        c = 0
+        for xy in xy_set:
+            tf = XForm(self.parent, name='{}_{}'.format(self.parent.name, c))
+            tf.translation.x = xy[0]
+            tf.translation.y = xy[1]
+
+            tfs.append(tf)
+
+            c += 1
+
+        return tfs
